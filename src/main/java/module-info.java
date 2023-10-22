6d358d7f98047c5d0296d9f33f53d66f4a13c9f5
Everton Russomanno <evertonmartins154@gmail.com>
2023-10-22 16:19:52 -0300

    Add DepartmentForm (dialog) design

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,15 @@ module com.evertonmartins.javafxjdbc {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.desktop;
 
     exports com.evertonmartins.javafxjdbc.application;
     opens com.evertonmartins.javafxjdbc.application to javafx.fxml;
 
     exports com.evertonmartins.javafxjdbc.model.dao.impl;
     opens com.evertonmartins.javafxjdbc.model.dao.impl to javafx.fxml;
+
+    exports com.evertonmartins.javafxjdbc.util;
+    opens com.evertonmartins.javafxjdbc.util to javafx.fxml;
 
 }
