4a435713de5936101d520396ad94142940c33a8e
Everton Russomanno <evertonmartins154@gmail.com>
2023-10-22 10:58:24 -0300

    Add About View

--- src/main/java/com/evertonmartins/javafxjdbc/application/MainViewController.java
+++ src/main/java/com/evertonmartins/javafxjdbc/application/MainViewController.java
@@ -1,9 +1,17 @@
 package com.evertonmartins.javafxjdbc.application;
 
+import com.evertonmartins.javafxjdbc.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -28,11 +36,28 @@ public class MainViewController implements Initializable {
 
     @FXML
     public void onMenuItemAboutAction(){
-        System.out.println("onMenuItemAboutAction");
+        loadView("/About.fxml");
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
 
+    private synchronized void loadView(String absoluteName){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVbox = loader.load();
+
+            Scene mainScene = Main.getMainScene();
+            VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+            Node mainMenu = mainVbox.getChildren().get(0);
+            mainVbox.getChildren().clear();
+            mainVbox.getChildren().add(mainMenu);
+            mainVbox.getChildren().addAll(newVbox.getChildren());
+
+        }catch (IOException e){
+            Alerts.showAlert("IOException", "Error loafing view", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
